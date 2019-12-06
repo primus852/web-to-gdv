e3ebeb09ee250203328208fe92bdb588a6dfcced
t.wolter <torsten.wolter@epikur.de>
2019-12-06 12:51:54 +0100

    Debug

--- src/Util/Gdv/Gdv.php
+++ src/Util/Gdv/Gdv.php
@@ -152,6 +152,7 @@ class Gdv
                 $y = 0;
                 foreach ($newNode->filter('Kommunikation') as $comm) {
 
+					dump($comm);die;
                 	if(!$comm->count()){
                 		dump('isEmpty');
                 		continue;
